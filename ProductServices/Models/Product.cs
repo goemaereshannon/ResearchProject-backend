08750d370948701a073c3c4b4ceaf3dc76d2cf96
Shannon <shannon.goemaere@student.howest.be>
2021-01-04 19:35:10 +0100

    Models

---
+++ ProductServices/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductServices.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; } = Guid.NewGuid(); 
+        public string Name { get; set; }
+        public bool ProductAvailable { get; set; } = true;
+
+        public Guid? CategoryId { get; set; }
+        public Guid? PriceId { get; set; }
+
+        public Category Category { get; set; }
+        public Price Price { get; set; }
+
+        public ICollection<ProductHasSize> ProductHasSizes { get; set; }
+
+    }
+}
---
+++ ProductServices/Models/ProductHasSize.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductServices.Models
+{
+    public class ProductHasSize
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public int Stock { get; set; }
+
+        public Guid ProductId { get; set; }
+        public Guid SizeId { get; set; }
+
+    }
+}
